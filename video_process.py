1aba2df2317c5f9eb4805481a016b8fe87192643
Ziv <zivlazarov@gmail.com>
2022-01-04 02:55:00 +0200

    added face recognition

--- video_process.py
+++ video_process.py
@@ -1,11 +1,9 @@
 import numpy as np
-import tensorflow as tf
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.models import Sequential
 
 import cv2 as cv
 
-face_cascade = cv.CascadeClassifier(cv.data.harrcascade + 'haarcascade_frontalface_default.xml')
+eyes_cascade = cv.CascadeClassifier('/Users/zivlazarov/Projects/RaspberryPika/eyes_recognition_model.xml')
+face_cascade = cv.CascadeClassifier('/Users/zivlazarov/Projects/RaspberryPika/faces_recognition_model.xml')
 
 # getting camera
 cap = cv.VideoCapture(0)
@@ -23,22 +21,34 @@ while True:
 		print("Can't receive frame. Exiting...")
 		break
 
-	gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-	gray_flip = cv.flip(gray, 1)
+	img_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	# gray_flip = cv.flip(gray, 1)
 	# 2 windows "mirrored"
 	# combined_window = np.hstack([gray, gray_flip])
 
-	faces = face_cascade.detectMultiScale(gray_flip, 1.3, 5)
+	# Getting corners around the face
+	faces = face_cascade.detectMultiScale(img_gray, 1.3, 5)
+   	# drawing bounding box around face
 	for (x, y, w, h) in faces:
-		frame = cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+   	    frame = cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+	# dogs = dog_cascade.detectMultiScale(img_gray, 1.3, 5)
+	# for (xx, yy, ww, hh) in dogs:
+	# 	frame = cv.rectangle(frame, (xx, yy), (xx + ww, yy + hh), (255, 0, 0), 3)
+
+	'''
+   	# detecting eyes
+	eyes = eyes_cascade.detectMultiScale(imgGray)
+   	# drawing bounding box for eyes
+	for (ex, ey, ew, eh) in eyes:
+   	    frame = cv.rectangle(frame, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 3)
+	'''
 
 	cv.imshow('frame', frame)
 
 	# listening for exit key which is 'q'
 	if cv.waitKey(1) == ord('q'):
 		break
-
-	# add object detection scripts here
 
 cap.release()
 cv.destroyAllWindows()
